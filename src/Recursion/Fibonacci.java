8427c1ea81de0c2c3467c64b10a7bc0c345fbea2
Avanish-29 <avanishdubey0002@gmail.com>
2025-04-16 19:53:15 +0530

    Solved Fibonacci Problem using recursion

--- src/Recursion/Fibonacci.java
+++ src/Recursion/Fibonacci.java
@@ -1,7 +1,18 @@
 package Recursion;
 
+import java.util.Scanner;
+
 public class Fibonacci {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = input.nextInt();
+        System.out.println("Fibonacci is: " + fibonacci(n));
+    }
+    public static int fibonacci(int n){
+        if(n<=1){
+            return n;
+        }
+        return fibonacci(n-1) + fibonacci(n-2);
     }
 }
