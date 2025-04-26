0fa540bd727f756b9cd4b6c633045168fa871d03
Avanish-29 <avanishdubey0002@gmail.com>
2025-04-26 10:34:10 +0530

    Solved first problem of Arrays

---
+++ src/Arrays/largestElement.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class largestElement {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of element in the array: ");
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        for (int i=0;i<n;i++){
+            System.out.println("Enter the value at "+i+"th position: ");
+            arr[i] = input.nextInt();
+        }
+        System.out.println("The Array is : " + Arrays.toString(arr));
+        System.out.println("The largest element of the array is: " + largest(arr));
+    }
+
+    public static int largest(int[] arr){
+        int largest = arr[0];
+        for (int i = 0; i <arr.length-1; i++) {
+            if(arr[i] > largest){
+                largest = arr[i];
+            }
+        }
+        return largest;
+    }
+}
