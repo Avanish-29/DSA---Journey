36dca90a8ce2c3efd57ee199478f67b069343913
Avanish-29 <avanishdubey0002@gmail.com>
2025-04-26 10:36:45 +0530

    Solved Array Question

--- src/Arrays/largestElement.java
+++ src/Arrays/largestElement.java
@@ -19,7 +19,7 @@ public class largestElement {
 
     public static int largest(int[] arr){
         int largest = arr[0];
-        for (int i = 0; i <arr.length-1; i++) {
+        for (int i = 0; i <arr.length; i++) {
             if(arr[i] > largest){
                 largest = arr[i];
             }
