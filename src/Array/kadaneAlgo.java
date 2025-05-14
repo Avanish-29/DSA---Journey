70e35f691547666e6b82ab41d5ac0144d87ed1c3
Avanish-29 <avanishdubey0002@gmail.com>
2025-05-14 10:12:39 +0530

    Updated the Kadane's algo for edge cases

--- src/Array/kadaneAlgo.java
+++ src/Array/kadaneAlgo.java
@@ -19,6 +19,9 @@ public class kadaneAlgo {
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             sum = sum + arr[i];
+            if(sum > max){
+                max = sum;
+            }
             if(sum < 0){
                 sum = 0;
             }
