515d4388fb92fba36dd1a99abd626f2900bf25bf
Avanish-29 <avanishdubey0002@gmail.com>
2025-05-23 20:15:31 +0530

    Solved peak element in the array...23/5/25...8:15PM

---
+++ src/Binary_Search/peakElement.java
@@ -0,0 +1,28 @@
+package Binary_Search;
+
+import java.util.Scanner;
+
+public class peakElement {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of element in the array : ");
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter the element at : " + i + "th position: ");
+            arr[i] = input.nextInt();
+        }
+        System.out.println("The peak element is "+arr[peak(arr)]+" which occurs at "+ peak(arr) + " index.");
+    }
+    public static int peak(int[] arr){
+        int start = 0, end = arr.length-1;
+        while(start<end){
+            int mid = start + (end-start)/2;
+            if(arr[mid]>arr[mid+1]){
+                end = mid;
+            }
+            else start = mid+1;
+        }
+        return end;
+    }
+}
