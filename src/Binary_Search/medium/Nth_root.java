b8d8e39f01a9a91e670584af5c0f7bdbd558593e
Avanish Dubey <155886061+Avanish-29@users.noreply.github.com>
2025-05-31 20:09:21 +0530

    Create Nth_root.java

---
+++ src/Binary_Search/medium/Nth_root.java
@@ -0,0 +1,8 @@
+public class Nth_root{
+public static void main(String[] args){
+Scanner input = new Scanner(System.in);
+System.out.println("Enter the number : );
+int n = input.nextInt();
+System.out.println(" Enter the target result:);
+}
+}
